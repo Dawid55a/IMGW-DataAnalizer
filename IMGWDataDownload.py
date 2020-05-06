5697761b3979dcbf09ad17b8c1938d92af1bced1
Dawid Kiełkowski <dawid55a@gmail.com>
2020-05-06 15:43:44 +0200

    Adding .gitignore and changed downloading to all files (for now), and added extracting
    Signed-off-by: Dawid55a <dawid55a@gmail.com>

--- IMGWDataDownload.py
+++ IMGWDataDownload.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
-import os
 import re
 from tqdm import tqdm
 from pathlib import Path
+from zipfile import ZipFile
+
 SITE_URL = r'https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/opad/'
 # showing possible dates
-print("You can choose from 01.2008 to 03.2020")
+# print("You can choose from 01.2008 to 03.2020")
 
 # connecting to site and mapping data
 r = requests.get(SITE_URL)
@@ -17,18 +18,32 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 # getting all years with data
 years = []
 for link in soup.find_all('a'):
-    adress = link.get('href')
-    # print(adress)
-    if re.search("[0-9]/$", adress) is not None:
-        # print(adress)
-        years.append(adress)
+    address = link.get('href')
+    # print(address)
+    if re.search("[0-9]/$", address) is not None:
+        # print(address)
+        years.append(address)
 print(f"All years: {years}")
+
+# years = ["2006/"]
 
 # TODO: --------------------Try tu use RegEx Dawid------------------------
 
 # raw_date = input("Give date range in format [mm-rrrr/mm-rrrr]: ")
 # TODO: choosing from when we want to download data
-date_range = {'2008': ['01', '02']}
+# date_range = {'2008': ['01', '02']}
+
+# home folder location
+download_folder = f"{Path.home()}/Downloads/IMGWDataDownload"
+
+try:
+    Path.mkdir(Path(f'{download_folder}/Extracted'))
+except FileExistsError:
+    # TODO: Checking existing files in case they were downloaded already
+    pass
+
+# list holding  names of all downloaded files
+downloaded_files = []
 
 for year in years:
     # print(f"{SITE_URL}/{year}")
@@ -36,16 +51,19 @@ for year in years:
     year_site = BeautifulSoup(r2.content, 'html.parser')
 
     for link in year_site.find_all('a'):
-        adress2 = link.get('href')
-        # print(adress2)
-        if re.search("zip$", adress2) is not None:
-            file = requests.get(f"{SITE_URL}/{year}/{adress2}", stream=True)
-            # print(file.headers)
+        zip_file = link.get('href')
+        if re.search("zip$", zip_file) is not None:
+            file = requests.get(f"{SITE_URL}/{year}/{zip_file}", stream=True)
 
-            path = f'{Path.home()}/Downloads'
-            with open(f'{path}/{adress2}', 'wb') as f:
+            with open(f'{download_folder}/{zip_file}', 'wb') as f:
                 total_size = int(r.headers['content-length'])
                 for data in tqdm(iterable=file.iter_content(chunk_size=1024), total=total_size / 1024, unit='KB'):
                     f.write(data)
-            print("Download complete!")
+            print(f"{zip_file}: Download complete!")
+            downloaded_files.append(zip_file)
 
+# extracting all downloaded files
+for file in downloaded_files:
+    with ZipFile(f'{download_folder}/{file}', 'r') as zip_ref:
+        zip_ref.extractall(f'{download_folder}/Extracted')
+print("Files extracted")
